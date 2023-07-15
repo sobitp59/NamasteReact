8a9eaa89b47557214b078f24a61fb612a723d712
sobitp59 <sobitp59@gmail.com>
2023-07-16 00:08:22 +0530

    E3 : laying the foundayion

--- nReact E2 - iginite the app/App.js
+++ nReact E2 - iginite the app/App.js
@@ -1,13 +1,21 @@
-const headings = React.createElement('div', {}, [
-    React.createElement('h1', { key : 'H1'}, 'I am H1'),
-    React.createElement('h2', { key : 'H2'}, 'I am H2'),
-    React.createElement('h3', { key : 'H3'}, 'I am H3'),
-    React.createElement('h4', { key : 'H4'}, 'I am H4'),
-    React.createElement('h5', { key : 'H5'}, 'I am H5'),
-    React.createElement('h6', { key : 'H6'}, 'I am H6'),
-] );
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const TitleComponent = () => (
+    <h1 className="title">I AM TITLE COMPONENT</h1>
+)
+
+const HeadingComponent = () => (
+    <div id='container'>
+        <TitleComponent />
+        <TitleComponent></TitleComponent>
+        {TitleComponent()}
+        <h1 className="heading">I AM HEADING COMPONENT 😎</h1>
+    </div>
+)
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(headings);
+root.render(<HeadingComponent />);
